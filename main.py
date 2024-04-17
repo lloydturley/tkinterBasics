9400787e521ccbc6a4aba736cc59a05796757b0f
Lloyd Turley <Lloyd.Turley@hyland.com>
2024-04-17 16:52:26 -0400

    another

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import tkinter
 
+def add(*args):
+    result=0
+    for n in args:
+        result+=n
+    return result
+
+res = add(2,4,2,4,3,2,4)
+
 window = tkinter.Tk()
 window.title("First GUI")
+
 window.minsize(width=500, height=500)
 
-my_label = tkinter.Label(text="Label 1", font=("Arial", 24, "bold"))
+my_label = tkinter.Label(text=res, font=("Arial", 24, "bold"))
 my_label.pack()
+
 
 
 
